f3942028a16f9310684e6ecb2859576b0da06ab5
zakriat <muhammadzakriatunio@gmail.com>
2025-01-25 00:19:38 +0500

    Initial release

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Laravel Deepseek R1 Integration
 
 [![Latest Version](https://img.shields.io/packagist/v/zakriat/laravel-deepseek-r1.svg?style=flat-square)](https://packagist.org/packages/zakriat/laravel-deepseek-r1)
+[![Tests](https://github.com/zakriat/laravel-deepseek-r1/actions/workflows/tests.yml/badge.svg)](https://github.com/zakriat/laravel-deepseek-r1/actions/workflows/tests.yml)
+
+Laravel package for integrating with Deepseek R1 AI API.
 
 ## Installation
 
--- tests/DeepseekTest.php
+++ tests/DeepseekTest.php
@@ -4,6 +4,9 @@ namespace Zakriat\DeepseekR1\Tests;
 
 use Zakriat\DeepseekR1\DeepseekClient;
 use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
 
 class DeepseekTest extends TestCase
 {
@@ -11,5 +14,25 @@ class DeepseekTest extends TestCase
     {
         $client = new DeepseekClient('test-key', 'https://api.deepseek.com/v1/');
         $this->assertInstanceOf(DeepseekClient::class, $client);
+    }
+
+    public function test_api_request()
+    {
+        $mock = new MockHandler([
+            new Response(200, [], json_encode([
+                'choices' => [['message' => ['content' => 'Test response']]]
+            ]))
+        ]);
+
+        $client = new DeepseekClient('test-key', 'https://api.deepseek.com/v1/', [
+            'handler' => HandlerStack::create($mock)
+        ]);
+
+        $response = $client->chatCompletion([
+            'messages' => [['role' => 'user', 'content' => 'Hello']]
+        ]);
+
+        $this->assertArrayHasKey('choices', $response);
+        $this->assertEquals('Test response', $response['choices'][0]['message']['content']);
     }
 }
